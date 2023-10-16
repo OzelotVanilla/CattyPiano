b00b1496d9d3eaecafb38e6f9c56ecc5625af514
Raelyn06 <zhachenl@kean.edu>
2023-10-16 11:20:49 +0800

    feature - piano keyboard implementation

--- pages/piano/PianoKeyboard.tsx
+++ pages/piano/PianoKeyboard.tsx
@@ -1,34 +1,70 @@
-"use client"
+import React from 'react';
 
-import { PossibleKey } from "@/utils/music"
+type PossibleKey = 'C' |'C#'|'D'|'D#'|'E'|'F'|'F#'|'G'|'G#'|'A'|'A#'|'B';
 
+type PianoKeyboardProps = {
+  number_of_keys?: number;
+  start_from?: PossibleKey;
+};
 
-type PianoKeyboard_Props = {
-    /**
-     * Integer number to control the number of
-     * piano keys to show.
-     */
-    number_of_keys?: number
+type PianoKeyProps = {
+  note: PossibleKey;
+  octave: number;
+  isSharp?: boolean;
+  audioSrc?: string;
+};
 
-    /**
-     * The generation
-     */
-    start_from?: PossibleKey
+function PianoKey({ note, octave, isSharp = false, audioSrc }: PianoKeyProps) {
+  const handleKeyDown = () => {
+    // Play audio if audioSrc is provided
+    if (audioSrc) {
+      const audio = new Audio(audioSrc);
+      audio.play();
+    }
+  };
+
+  return (
+    <div
+      className={`piano-key ${isSharp ? 'sharp' : 'natural'}`}
+      onMouseDown={handleKeyDown}
+    >
+      {`${note}${octave}`}
+    </div>
+  );
 }
 
 export function PianoKeyboard({
-    number_of_keys = 12, start_from = "C"
-}: PianoKeyboard_Props)
-{
-    return (<></>)
-}
+  number_of_keys = 12,
+  start_from = 'C',
+}: PianoKeyboardProps) {
+  const pianoNotes: PossibleKey[] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+  const octaveCount = Math.ceil(number_of_keys / pianoNotes.length);
 
-function PianoWhiteKey()
-{
+  const renderKeys = () => {
+    const keys: JSX.Element[] = [];
 
-}
+    for (let octave = 0; octave < octaveCount; octave++) {
+      for (const note of pianoNotes) {
+        const isSharp = note === 'E' || note === 'B'; // Determine if the key is sharp
 
-function PianoBlackKey()
-{
+        keys.push(
+          <PianoKey
+            key={`${note}-${octave}`}
+            note={note}
+            octave={octave}
+            isSharp={isSharp}
+            audioSrc={/* Add appropriate audio source for the note */}
+          />
+        );
+      }
+    }
 
+    return keys;
+  };
+
+  return (
+    <div className="piano-keyboard">
+      {renderKeys()}
+    </div>
+  );
 }
