1f1a5e4768e581332c01484adc15e7ba52228f0d
Ozelot Vanilla <shuihenyun@qq.com>
2024-01-16 00:26:42 +0800

    util - `shiftToRange` with test

---
+++ tests/utils.test.ts
@@ -0,0 +1,13 @@
+import { shiftToRange } from "../utils/music"
+
+test(
+    "Check `shiftToRange` in `music.ts`",
+    function ()
+    {
+        const toC3A3Range = (n: number) => shiftToRange(n, 48, 59)
+        expect(toC3A3Range(50)).toBe(50)
+        expect(toC3A3Range(47)).toBe(59)
+        expect(toC3A3Range(36)).toBe(48)
+        expect(toC3A3Range(78)).toBe(54)
+    }
+)
--- utils/music.ts
+++ utils/music.ts
@@ -45,3 +45,41 @@ export function isSharpKey(midi_note_num: number)
 {
     return midi_note_to_name[midi_note_num]?.includes("#")
 }
+
+/**
+ * Shift a note to fit in the range (can be reversed).
+ * 
+ * If the given range is too narrow to contain at least one octave, a `RangeError` will be thrown.
+ * 
+ * Example: given keyboard range from `C3` to `A3`, `C5` will be shifted to `C3`, and `D2` will be `D3`.
+ * 
+ * @param midi_note_num The note going to be shifted.
+ * @param start_num Start of the given range, inclusive.
+ * @param end_num End of the given range, inclusive.
+ */
+export function shiftToRange(midi_note_num: number, start_num: number, end_num: number)
+{
+    if (!Number.isInteger(midi_note_num)) { throw TypeError(`Note number ${midi_note_num} is not an integer!`) }
+    if (midi_note_num < 21 || midi_note_num > 108) { throw RangeError(`Note number ${midi_note_num} out of range.`) }
+
+    // Accept reversed range, just swap them.
+    if (start_num > end_num) { const t = start_num; start_num = end_num; end_num = t }
+
+    // If it is too narrow to contain one octave, throw `RangeError`.
+    if (end_num - start_num < 11)
+    {
+        throw RangeError(
+            `Range too narrow, from ${convertNoteNumToKeyName(start_num)} (${start_num}) ` +
+            `to ${convertNoteNumToKeyName(end_num)} (${end_num}) does not contains an octave.`
+        )
+    }
+
+    // If the note is just inside the range, just return.
+    if (start_num <= midi_note_num && midi_note_num <= end_num) { return midi_note_num }
+    else // The note should be shifted into the given range.
+    {
+        const octave_difference = Math.floor((midi_note_num - start_num) / 12)
+
+        return midi_note_num - 12 * octave_difference
+    }
+}
