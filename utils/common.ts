eede4048468e2914f809d13e32c9764eef013144
Ozelot Vanilla <shuihenyun@qq.com>
2024-01-25 22:09:31 +0800

    feature - Support `jsonc` song file

--- utils/common.ts
+++ utils/common.ts
@@ -2,9 +2,13 @@ import { Time as convertToTime, getTransport } from "tone";
 import { Time } from "tone/build/esm/core/type/Units";
 
 export const jsonfyResponse =
-    (r: Response) =>
-        r.json()
-            .catch(reason => console.error(`Requested URL ${r.url} does not contains valid JSON.`, reason))
+    async (r: Response) =>
+    {
+        const text = await r.text()
+
+        try { return JSON.parse(text.replace(/\/\*\*?(?:.|\n|\r|\r\n)*\*\/|\/\/.*$/gm, "")) }
+        catch (reason) { console.error(`Requested URL ${r.url} does not contains valid JSON.`, reason) }
+    }
 
 export function convertToSeconds(t: Time)
 {
